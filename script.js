5166962f14912fee23f45b86b3a82b08f6a87591
kayckhirt <kayckhirt15@gmail.com>
2022-07-13 19:31:47 -0300

    Estilizacao: feito a estilizacao do header e comeco do formulario

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonLogin = document.querySelector('.entrar');
 const checkboxAgreement = document.querySelector('#agreement');
 
+
 function validaLogin() {
   const login = document.querySelector('.email');
   const senha = document.querySelector('.senha');
@@ -20,5 +21,18 @@ function validaCheckbox() {
   }
 }
 
+const areaDeTexto = document.querySelector('#textarea');
+
+
+function decrementaCaracteres() {
+  const contador = document.querySelector('#counter');
+  let valorText = areaDeTexto.value.length;
+  let tamanhoTexto = valorText + 1;
+  contador.innerText = 500 - valorText;
+  
+}
+console.log(areaDeTexto)
+
 buttonLogin.addEventListener('click', validaLogin);
 checkboxAgreement.addEventListener('click', validaCheckbox);
+areaDeTexto.addEventListener('keypress', decrementaCaracteres)
